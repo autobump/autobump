af311599183c5050f87d6e6dd73b94a803c1de53
glenn schrooyen <gschrooyen@gmail.com>
2020-04-22 13:44:06 +0200

    more tests

--- src/test/java/model/DependencyTest.java
+++ src/test/java/model/DependencyTest.java
@@ -4,6 +4,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class DependencyTest {
     private Dependency dependency;
@@ -31,5 +32,26 @@ public class DependencyTest {
     @Test
     public void testToString() {
         assertEquals("Dependency(group=test, name=test, version=test)", dependency.toString());
+    }
+
+    @Test
+    public void testEquals() {
+        Dependency dependency = new Dependency("test", "test", "test");
+        Dependency dependency1 = new Dependency(null, "test", "test");
+        Dependency dependency2 = new Dependency("test", null, "test");
+        Dependency dependency3 = new Dependency("test", "test", null);
+        assertEquals(dependency, dependency);
+        assertNotEquals(dependency, dependency1);
+        assertNotEquals(dependency, dependency2);
+        assertNotEquals(dependency, dependency3);
+
+    }
+
+    @Test
+    public void testHashCode() {
+    }
+
+    @Test
+    public void testToString1() {
     }
 }
