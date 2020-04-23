f3e6db5c2f76fe1a312fe5c7b27d9e91f9c60300
GrietVermeesch <griet.vermeesch@gmail.com>
2020-04-23 11:26:37 +0200

    fixed serialversionUID

--- src/main/java/exceptions/DependencyParserException.java
+++ src/main/java/exceptions/DependencyParserException.java
@@ -1,7 +1,8 @@
 package exceptions;
 
 public class DependencyParserException extends RuntimeException{
-    private static final long serialVersionUID = 4L;
+
+    private static final long serialVersionUID = -1062390353272772788L;
 
     public DependencyParserException(String s, Exception cause){
         super(s, cause);
--- src/main/java/exceptions/NoDependencyFileFoundException.java
+++ src/main/java/exceptions/NoDependencyFileFoundException.java
@@ -1,7 +1,8 @@
 package exceptions;
 
 public class NoDependencyFileFoundException extends RuntimeException {
-    private static final long serialVersionUID = 4L;
+
+    private static final long serialVersionUID = 3426608271051128854L;
 
     public NoDependencyFileFoundException(String s, Exception cause) {
         super(s, cause);
