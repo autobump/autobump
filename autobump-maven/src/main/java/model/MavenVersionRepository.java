90817bfb4e375f8c14e6526fbffda41baf121e16
glenn schrooyen <gschrooyen@gmail.com>
2020-04-23 14:15:38 +0200

    findng a version

---
+++ autobump-maven/src/main/java/model/MavenVersionRepository.java
@@ -0,0 +1,58 @@
+package model;
+
+import exceptions.DependencyParserException;
+import exceptions.WrongUrlException;
+import org.apache.maven.artifact.repository.metadata.io.xpp3.MetadataXpp3Reader;
+import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UncheckedIOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class MavenVersionRepository implements VersionRepository {
+    private static final String REPO_URL = "https://repo1.maven.org/maven2/";
+    private static final String URL_SUFFIX = "maven-metadata.xml";
+
+    @Override
+    public Set<Version> getAllAvailableVersions(Dependency dependency) {
+        try(BufferedReader in =
+                    new BufferedReader(
+                            new InputStreamReader(
+                                    getRepoUri(dependency)
+                                            .openConnection()
+                                            .getInputStream()
+                            )
+                    )
+        ) {
+            return new MetadataXpp3Reader()
+                    .read(in)
+                    .getVersioning()
+                    .getVersions()
+                    .stream()
+                        .map(Version::new)
+                        .collect(Collectors.toSet());
+        } catch (IOException e) {
+            throw new UncheckedIOException("wrong url", e);
+        } catch (XmlPullParserException e) {
+            throw new DependencyParserException("something went wrong while parseing the xml", e);
+        }
+    }
+
+    private URL getRepoUri(Dependency dependency) {
+        try {
+            return new URL(MavenVersionRepository.REPO_URL +
+                    dependency.getGroup().replace(".", "/") +
+                    "/" +
+                    dependency.getName() +
+                    "/" +
+                    MavenVersionRepository.URL_SUFFIX);
+        } catch (MalformedURLException e) {
+            throw new WrongUrlException("wrong URI Syntax", e);
+        }
+    }
+}
