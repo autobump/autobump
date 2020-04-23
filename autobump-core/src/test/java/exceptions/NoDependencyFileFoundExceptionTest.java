dede606597c98ef84ad7e1ad0b6b48517ec3cfe2
glenn schrooyen <gschrooyen@gmail.com>
2020-04-23 12:03:36 +0200

    cleanup

--- autobump-core/src/test/java/exceptions/NoDependencyFileFoundExceptionTest.java
+++ autobump-core/src/test/java/exceptions/NoDependencyFileFoundExceptionTest.java
@@ -2,7 +2,7 @@ package exceptions;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class NoDependencyFileFoundExceptionTest {
 
