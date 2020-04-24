adba4e324565833fc1997cf37a05e56c33e25715
glenn schrooyen <gschrooyen@gmail.com>
2020-04-24 15:25:30 +0200

    test

---
+++ autobump-jgit/src/test/java/com/github/autobump/jgit/model/JGitGitClientTest.java
@@ -0,0 +1,17 @@
+package com.github.autobump.jgit.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JGitGitClientTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void testClone() {
+    }
+}
