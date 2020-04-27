9734c87c74c25c03ee548da2d5a152d011502f82
glenn schrooyen <gschrooyen@gmail.com>
2020-04-27 13:07:26 +0200

    cleanup

--- autobump-jgit/src/main/java/com/github/autobump/jgit/model/JGitGitClient.java
+++ autobump-jgit/src/main/java/com/github/autobump/jgit/model/JGitGitClient.java
@@ -7,22 +7,19 @@ import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.lib.Repository;
 
-import java.io.File;
+import java.io.IOException;
 import java.net.URI;
-import java.util.UUID;
+import java.nio.file.Files;
 
 public class JGitGitClient implements GitClient {
     @Override
     public Workspace clone(URI uri) {
         try(Repository repo = Git.cloneRepository().setURI(uri.toString())
-                .setDirectory(
-                        new File(System.getProperty("java.io.tmpdir") +
-                                        File.separator +
-                                        UUID.randomUUID().toString()))
+                .setDirectory(Files.createTempDirectory("cloned_repos").toFile())
                 .call().getRepository()) {
 
             return new Workspace(repo.getDirectory().getPath().replace(".git", ""));
-        } catch (GitAPIException e) {
+        } catch (GitAPIException | IOException e) {
             throw new GitException("something went wrong while cloneing the repo", e);
         }
     }
