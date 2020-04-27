db6254e3f710097e1ea76e275186f4fea06ba03b
glenn schrooyen <gschrooyen@gmail.com>
2020-04-27 17:30:00 +0200

    cleanup

--- autobump-maven/src/main/java/com/github/autobump/maven/exceptions/DependencyNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.github.autobump.maven.exceptions;
-
-public class DependencyNotFoundException extends RuntimeException {
-    private static final long serialVersionUID = 8611954154370624230L;
-
-    public DependencyNotFoundException(String message) {
-        super(message);
-    }
-}
