04218837edd77c5dc7bd2b23aaaa8d520b1f3d86
glenn schrooyen <gschrooyen@gmail.com>
2020-04-27 12:11:12 +0200

    cleanup

---
+++ autobump-core/src/test/java/com/github/autobump/core/model/WorkspaceTest.java
@@ -0,0 +1,27 @@
+package com.github.autobump.core.model;
+
+import com.github.autobump.core.exceptions.NoDependencyFileFoundException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class WorkspaceTest {
+    private transient Workspace workspace;
+
+    @BeforeEach
+    void setUp() {
+        workspace = new Workspace("src/test/resources/project_root");
+    }
+
+    @Test
+    void getDependencyDocument() {
+        assertNotNull(workspace.getDependencyDocument("pom.xml"));
+    }
+
+    @Test
+    void wrongDependencytest() {
+        assertThrows(NoDependencyFileFoundException.class, () -> workspace.getDependencyDocument("build.gradle"));
+    }
+}
