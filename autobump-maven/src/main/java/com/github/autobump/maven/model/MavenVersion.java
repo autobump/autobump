58af5649a9f3c449ddc9b76fd0af8c3f8c1c8165
GrietVermeesch <47347973+GrietVermeesch@users.noreply.github.com>
2020-04-28 11:41:24 +0200

    implemented comparison of versions (#68)
    * implemented comparison of versions
    
    * removed code smell
    
    * removed author tags
    
    * updated maven-artifact dependency and cleanup
    
    Co-authored-by: GrietVermeesch <griet.vermeesch@gmail.com>

---
+++ autobump-maven/src/main/java/com/github/autobump/maven/model/MavenVersion.java
@@ -0,0 +1,16 @@
+package com.github.autobump.maven.model;
+
+import com.github.autobump.core.model.Version;
+import lombok.Value;
+import org.apache.maven.artifact.versioning.ComparableVersion;
+
+@Value
+public class MavenVersion implements Version {
+    String versionNumber;
+
+    @Override
+    public int compareTo(Version o) {
+        ComparableVersion comparableVersion = new ComparableVersion(versionNumber);
+        return comparableVersion.compareTo(new ComparableVersion(o.getVersionNumber()));
+    }
+}
