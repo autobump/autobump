4ba00f0781eea6f4a965b01645204ade33a8bd25
Jos <36787145+the-jos@users.noreply.github.com>
2020-04-28 17:50:51 +0200

    httpclient refactor

--- autobump-maven/src/main/java/com/github/autobump/maven/model/MavenVersionRepository.java
+++ autobump-maven/src/main/java/com/github/autobump/maven/model/MavenVersionRepository.java
@@ -9,17 +9,25 @@ import lombok.Getter;
 import org.apache.maven.artifact.repository.metadata.io.xpp3.MetadataXpp3Reader;
 import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.InputStream;
 import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 
 @Getter
 public class MavenVersionRepository implements VersionRepository {
+    private static final int STATUSCODE_OK = 200;
+    private static final int TIMEOUT = 30;
     private final String baseUrl;
 
     public MavenVersionRepository(String baseUrl) {
@@ -28,15 +36,8 @@ public class MavenVersionRepository implements VersionRepository {
 
     @Override
     public Set<Version> getAllAvailableVersions(Dependency dependency) {
-        try(BufferedReader in =
-                    new BufferedReader(
-                            new InputStreamReader(
-                                    getRepoUri(dependency)
-                                            .openConnection()
-                                            .getInputStream()
-                            )
-                    )
-        ) {
+        InputStream in = getInputStream(dependency);
+        try {
             return new MetadataXpp3Reader()
                     .read(in)
                     .getVersioning()
@@ -46,18 +47,31 @@ public class MavenVersionRepository implements VersionRepository {
                     .collect(Collectors.toUnmodifiableSet());
         } catch (XmlPullParserException e) {
             throw new DependencyParserException("something went wrong while parseing the xml", e);
-        }catch (IOException e) {
+        } catch (IOException e) {
             return new HashSet<>();
         }
     }
 
-    private URL getRepoUri(Dependency dependency) {
+    private InputStream getInputStream(Dependency dependency) {
+        HttpClient httpClient = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().GET().uri(getRepoUri(dependency))
+                .timeout(Duration.ofSeconds(TIMEOUT)).build();
+
+        CompletableFuture<HttpResponse<InputStream>> future = httpClient.sendAsync(request, responseInfo
+                -> responseInfo.statusCode() == STATUSCODE_OK ?
+                HttpResponse.BodySubscribers.ofInputStream() :
+                HttpResponse.BodySubscribers.replacing(null)
+        );
+        return future.thenApply(HttpResponse::body).join();
+    }
+
+    private URI getRepoUri(Dependency dependency) {
         try {
             return new URL(String.format("%s/%s/%s/maven-metadata.xml",
                     baseUrl,
                     dependency.getGroup().replaceAll("\\.", "/"),
-                    dependency.getName()));
-        } catch (MalformedURLException e) {
+                    dependency.getName())).toURI();
+        } catch (MalformedURLException | URISyntaxException e) {
             throw new WrongUrlException("wrong URI Syntax", e);
         }
     }
