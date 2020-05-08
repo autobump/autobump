7051400fdd470d8e2208fad0e01b3aa282618be7
Jos <36787145+the-jos@users.noreply.github.com>
2020-05-08 17:43:12 +0200

    ignore dependencies

--- autobump-maven/src/main/java/com/github/autobump/maven/model/MavenVersion.java
+++ autobump-maven/src/main/java/com/github/autobump/maven/model/MavenVersion.java
@@ -14,4 +14,27 @@ public class MavenVersion implements Version {
         ComparableVersion comparableVersion = new ComparableVersion(versionNumber);
         return new ComparableVersion(o.getVersionNumber()).compareTo(comparableVersion);
     }
+
+    @Override
+    public UpdateType getUpdateType(Version otherVersion) {
+        var explodedCurrent = getVersionNumber().split("\\.");
+        var explodedOther = otherVersion.getVersionNumber().split("\\.");
+        UpdateType updateType = UpdateType.NONE;
+        int size = Math.min(explodedCurrent.length, explodedOther.length);
+
+        if (explodedCurrent[0].compareTo(explodedOther[0]) < 0) {
+            updateType = UpdateType.MAJOR;
+        } else if (size > 1 && explodedCurrent[1].compareTo(explodedOther[1]) < 0) {
+            updateType = UpdateType.MINOR;
+        }
+
+        for (int i = 2; i < size; i++) {
+            if (explodedCurrent[i].compareTo(explodedOther[i]) < 0) {
+                updateType = UpdateType.INCREMENTAL;
+                break;
+            }
+        }
+
+        return updateType;
+    }
 }
