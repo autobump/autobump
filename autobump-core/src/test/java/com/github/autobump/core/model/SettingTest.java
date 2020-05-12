5c245e07a8d92a68b79713555b2ceb88d96bbb67
glenn schrooyen <gschrooyen@gmail.com>
2020-05-12 09:44:08 +0200

    cleanup

--- autobump-core/src/test/java/com/github/autobump/core/model/SettingTest.java
+++ autobump-core/src/test/java/com/github/autobump/core/model/SettingTest.java
@@ -3,7 +3,6 @@ package com.github.autobump.core.model;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class SettingTest {
 
