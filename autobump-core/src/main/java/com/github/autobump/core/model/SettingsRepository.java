12855bb2df8a4c8ac7fd53636950a0fd2581657f
glenn schrooyen <gschrooyen@gmail.com>
2020-05-12 09:34:28 +0200

    setting and settingsrepo

---
+++ autobump-core/src/main/java/com/github/autobump/core/model/SettingsRepository.java
@@ -0,0 +1,12 @@
+package com.github.autobump.core.model;
+
+import java.util.List;
+
+public interface SettingsRepository {
+
+    List<Setting> getSettings();
+
+    Setting saveSetting(Setting setting);
+
+    List<Setting> saveAllSettings(List<Setting> settings);
+}
