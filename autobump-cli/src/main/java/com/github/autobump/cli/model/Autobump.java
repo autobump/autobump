3fb7a1a4f30da309ad3252a3c00b255ef00cb7fe
Jos <36787145+the-jos@users.noreply.github.com>
2020-05-12 10:24:35 +0200

    Cli PropertiesProvider

--- autobump-cli/src/main/java/com/github/autobump/cli/model/Autobump.java
+++ autobump-cli/src/main/java/com/github/autobump/cli/model/Autobump.java
@@ -17,41 +17,21 @@ import com.github.autobump.maven.model.MavenDependencyResolver;
 import com.github.autobump.maven.model.MavenIgnoreRepository;
 import com.github.autobump.maven.model.MavenVersionRepository;
 import picocli.CommandLine;
-import picocli.CommandLine.Command;
 import picocli.CommandLine.Model.CommandSpec;
-import picocli.CommandLine.Option;
 
-import java.net.URI;
-import java.util.Map;
 import java.util.concurrent.Callable;
 
-@Command(
-        name = "autobump",
-        description = "automatically creates a pull-request for every outdated dependency in a project"
-)
 public class Autobump implements Callable<AutobumpResult> {
     @CommandLine.Spec
     private static CommandSpec spec;
+    @CommandLine.Mixin
+    AutobumpPropertiesProvider properties = AutobumpPropertiesProvider.getInstance();
     private final DependencyResolver dependencyResolver = new MavenDependencyResolver();
     private final DependencyBumper dependencyBumper = new MavenDependencyBumper();
     private VersionRepository versionRepository;
     private GitClient gitClient;
     private GitProvider gitProvider;
     private IgnoreRepository ignoreRepository;
-    @Option(names = {"-u", "--username"}, description = "User name for your remote repository", required = true)
-    private String username;
-    @Option(names = {"-l", "--url"}, paramLabel = "REPOURL", description = "project repository url", required = true)
-    private URI url;
-    @Option(names = {"-p", "--password"}, description = "Password for your remote repository", required = true)
-    private String password;
-    @Option(names = {"-r", "--repourl"}, description = "Public repositoryUrl for dependency version information",
-            defaultValue = "https://repo1.maven.org/maven2")
-    private String repositoryUrl;
-    @Option(names = {"-a", "--apiurl"}, description = "apiUrl", defaultValue = "https://api.bitbucket.org/2.0")
-    private String apiUrl;
-    @Option(names = {"-i", "--ignored"}, description = "Dependencies to ignore for updates including update" +
-            " type separated by comma", split = ",")
-    private Map<String,String> ignoreDependencies;
 
     public static void main(String[] args) {
         CommandLine cmd = new CommandLine(new Autobump());
@@ -67,11 +47,11 @@ public class Autobump implements Callable<AutobumpResult> {
     }
 
     private void initialize() {
-        versionRepository = new MavenVersionRepository(repositoryUrl);
-        gitClient = new JGitGitClient(username, password);
-        BitBucketAccount bitBucketAccount = new BitBucketAccount(username, password);
-        gitProvider = new BitBucketGitProvider(bitBucketAccount, apiUrl);
-        ignoreRepository = new MavenIgnoreRepository(ignoreDependencies);
+        versionRepository = new MavenVersionRepository(properties.getRepositoryUrl());
+        gitClient = new JGitGitClient(properties.getUsername(), properties.getPassword());
+        BitBucketAccount bitBucketAccount = new BitBucketAccount(properties.getUsername(), properties.getPassword());
+        gitProvider = new BitBucketGitProvider(bitBucketAccount, properties.getApiUrl());
+        ignoreRepository = new MavenIgnoreRepository(properties.getIgnoreDependencies());
     }
 
     public AutobumpUseCase getAutobumpUseCase() {
@@ -81,7 +61,7 @@ public class Autobump implements Callable<AutobumpResult> {
                 .gitClient(gitClient)
                 .gitProvider(gitProvider)
                 .urlHelper(new BitBucketUrlHelper())
-                .uri(url)
+                .uri(properties.getUrl())
                 .versionRepository(versionRepository)
                 .ignoreRepository(ignoreRepository)
                 .build();
