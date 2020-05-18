7792d32f17afc54333ab4fcf0240975cfdd18d8b
glenn schrooyen <gschrooyen@gmail.com>
2020-05-18 11:46:51 +0200

    test working

--- autobump-spring-boot/src/main/java/com/github/autobump/springboot/AutobumpSpringBoot.java
+++ autobump-spring-boot/src/main/java/com/github/autobump/springboot/AutobumpSpringBoot.java
@@ -2,8 +2,12 @@ package com.github.autobump.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.github.autobump")
+@EntityScan("com.github.autobump")
 public class AutobumpSpringBoot {
     public static void main(String[] args) {
         SpringApplication.run(AutobumpSpringBoot.class, args);
--- autobump-spring-boot/src/test/java/com/github/autobump/springboot/repositories/SpringSettingsRepositoryTest.java
+++ autobump-spring-boot/src/test/java/com/github/autobump/springboot/repositories/SpringSettingsRepositoryTest.java
@@ -4,13 +4,19 @@ import com.github.autobump.core.model.Setting;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+
 @SpringBootTest
+@EnableJpaRepositories
 class SpringSettingsRepositoryTest {
     @Autowired
-    private SpringSettingsRepository springSettingsRepository;
+    private JpaRepository jpaRepository;
 
     @Test
     void saveSetting() {
@@ -19,10 +25,21 @@ class SpringSettingsRepositoryTest {
                 .value("Minor")
                 .type(Setting.SettingsType.IGNORE)
                 .build();
-        assertThat(springSettingsRepository.saveSetting(setting)).isEqualToComparingFieldByField(setting);
+        assertThat(jpaRepository.save(setting)).isEqualToComparingFieldByField(setting);
     }
 
     @Test
     void saveAllSettings() {
+        var setting = Setting.builder()
+                .key("com.h2database:h2:1.4.200")
+                .value("Minor")
+                .type(Setting.SettingsType.IGNORE)
+                .build();
+        var setting2 = Setting.builder()
+                .key("com.h2database:h2:1.4.199")
+                .value("Major")
+                .type(Setting.SettingsType.IGNORE)
+                .build();
+        assertThat(jpaRepository.saveAll(List.of(setting, setting2)).containsAll(List.of(setting, setting2)));
     }
 }
