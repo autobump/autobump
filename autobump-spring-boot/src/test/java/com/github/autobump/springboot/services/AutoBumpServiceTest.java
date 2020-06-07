c387d0b3b45ccbd57e7f88433c993e2004b99785
GrietVermeesch <griet.vermeesch@gmail.com>
2020-06-07 18:03:49 +0200

    added dummy setting

--- autobump-spring-boot/src/test/java/com/github/autobump/springboot/services/AutoBumpServiceTest.java
+++ autobump-spring-boot/src/test/java/com/github/autobump/springboot/services/AutoBumpServiceTest.java
@@ -1,17 +1,48 @@
 package com.github.autobump.springboot.services;
 
+import com.atlassian.connect.spring.AtlassianHost;
+import com.atlassian.connect.spring.AtlassianHostRepository;
+import com.github.autobump.bitbucket.model.BitBucketGitProviderUrlHelper;
+import com.github.autobump.core.model.DependencyBumper;
+import com.github.autobump.core.model.DependencyResolver;
+import com.github.autobump.core.model.GitClient;
+import com.github.autobump.core.model.GitProvider;
+import com.github.autobump.core.model.GitProviderUrlHelper;
+import com.github.autobump.core.model.IgnoreRepository;
+import com.github.autobump.core.model.Repo;
+import com.github.autobump.core.model.RepoRepository;
+import com.github.autobump.core.model.Setting;
+import com.github.autobump.core.model.SettingsRepository;
+import com.github.autobump.core.model.UseCaseConfiguration;
+import com.github.autobump.core.model.VersionRepository;
+import com.github.autobump.springboot.configuration.Autobumpconfig;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.system.CapturedOutput;
 import org.springframework.boot.test.system.OutputCaptureExtension;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.when;
 
 @ExtendWith({SpringExtension.class, MockitoExtension.class, OutputCaptureExtension.class})
 @SpringBootTest
 @ActiveProfiles("test")
 class AutoBumpServiceTest {
-    /*@Autowired
+    @Autowired
     private AtlassianHostRepository repository;
 
     @Mock
@@ -20,14 +51,31 @@ class AutoBumpServiceTest {
     @Mock
     private RepoRepository repoRepository;
 
+    @Mock
+    private SettingsRepository settingsRepository;
+
     @Autowired
     @InjectMocks
     private AutoBumpService testService;
 
+    private Setting setting;
+
     @BeforeEach
     void setUp() {
         testService.setRepoRepository(repoRepository);
+        testService.setSettingsRepository(settingsRepository);
         var provider = Mockito.mock(GitProvider.class);
+        testService.setAutobumpconfig(autobumpconfig);
+        testService.setRepository(repository);
+        setting = Setting.builder()
+                .repositoryName("MultiModuleMavenProject")
+                .key("cron")
+                .type(Setting.SettingsType.CRON)
+                .value("true")
+                .build();
+        lenient().when(settingsRepository
+                .getCronSetting("MultiModuleMavenProject"))
+                .thenReturn(setting);
         lenient().when(provider.getRepos()).thenReturn(getDummyRepoList());
         lenient().when(autobumpconfig.getGitProvider()).thenReturn(provider);
     }
@@ -59,8 +107,9 @@ class AutoBumpServiceTest {
         return repo2;
     }
 
-    /*@Test
+    @Test
     void testRuntimeException(CapturedOutput log) {
+        makeStubs();
         var host = new AtlassianHost();
         host.setClientKey("test");
         host.setSharedSecret("test");
@@ -94,5 +143,5 @@ class AutoBumpServiceTest {
                 .dependencyResolver(Mockito.mock(DependencyResolver.class))
                 .dependencyBumper(Mockito.mock(DependencyBumper.class))
                 .build());
-    }*/
+    }
 }
